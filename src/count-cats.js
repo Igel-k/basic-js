0b64ac2592a3e9b29264f51ee6a9e1e4d3af0a0b
Karina Sedova <karina.sed@yandex.ru>
2025-11-04 12:33:55 +0300

    feat: solve count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,12 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
+function countCats(matrix) {
   // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+  const flatArray = matrix.flat();
+  const cats = flatArray.filter(item => item === '^^');
+  
+  return cats.length;
 }
 
 module.exports = {
